8cd6928e8feec64d7584ca6249280080b825ec64
NayemaU <87430369+NayemaU@users.noreply.github.com>
2022-03-10 19:17:29 +0000

    Add files via upload

---
+++ play_nayma.py
@@ -0,0 +1,26 @@
+
+# 1) Give me the lowest price from the list
+# 2) Remove the last item from the list
+
+list_price = [5.99, 6.0, 10, 99, 4.99, 4.88]
+
+# 1) Give me total price from movie subscription
+# Give me the cheapest move subscription
+
+movie_subscription = [
+        {
+            "movie_subscription_id": 1,
+            "subscription_name": "Disney",
+            "price": 4.99 
+        },
+        {
+            "movie_subscription_id": 2,
+            "subscription_name": "Netflix",
+            "price": 5.99 
+        },
+        {
+            "movie_subscription_id": 3,
+            "subscription_name": "Amazon Prime",
+            "price": 6.00
+        }
+]
